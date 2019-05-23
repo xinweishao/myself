7d784d300302fcf99135fc38b3b694fe743eb477
shaoxinwei <xinweishao@sohu-inc.com>
2019-05-23 10:20:07 +0800

    first

---
+++ nicolas-douding-demo/src/main/java/com.nicolas.common/test.java
@@ -0,0 +1,12 @@
+package com.nicolas.common;
+
+/**
+ * @version V1.0
+ *          注意：本内容仅限于搜狐新闻，禁止外泄以及用于其他的商业目
+ * @Package com.nicolas.common
+ * @Description: 用一句话描述该文件做什么)
+ * @author: 搜狐 - 新闻客户端 - 云端业务 - shaoxinwei
+ * @date: 2019-05-23 10:19
+ */
+public class test {
+}
