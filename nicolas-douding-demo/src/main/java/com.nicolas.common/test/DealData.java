a12f53d265b22836fe5b46110be62a51b555eee8
shaoxinwei <xinweishao@sohu-inc.com>
2019-05-28 17:50:27 +0800

    1

---
+++ nicolas-douding-demo/src/main/java/com.nicolas.common/test/DealData.java
@@ -0,0 +1,34 @@
+package com.nicolas.common.test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * @version V1.0
+ *          注意：本内容仅限于搜狐新闻，禁止外泄以及用于其他的商业目
+ * @Package com.nicolas.common.test
+ * @Description: 用一句话描述该文件做什么)
+ * @author: 搜狐 - 新闻客户端 - 云端业务 - shaoxinwei
+ * @date: 2019-05-28 17:49
+ */
+public class DealData implements Callable<Map<String, String>> {
+
+    private int fileIndex = 0;
+
+    public DealData(int fileIndex) {
+        this.fileIndex = fileIndex;
+    }
+
+    /**
+     * 任务的具体过程，一旦任务传给ExecutorService的submit方法，则该方法自动在一个线程上执行。
+     *
+     * @return
+     * @throws Exception
+     */
+    public Map<String, String> call() throws Exception {
+
+        Map<String, String> errorMessageMap = new HashMap<String, String>();
+        return errorMessageMap;
+    }
+}
---
+++ nicolas-douding-demo/src/main/java/com.nicolas.common/test/TreadPoolTest.java
@@ -0,0 +1,51 @@
+package com.nicolas.common.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @version V1.0
+ *          注意：本内容仅限于搜狐新闻，禁止外泄以及用于其他的商业目
+ * @Package com.nicolas.common.test
+ * @Description: 用一句话描述该文件做什么)
+ * @author: 搜狐 - 新闻客户端 - 云端业务 - shaoxinwei
+ * @date: 2019-05-28 17:48
+ */
+public class TreadPoolTest {
+    //线程池
+    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
+    public static void main(String[] args) {
+
+//        getAppTokenInfo("fb704ef691f49bab414ac3a08a86ca7864419437");
+
+        try {
+            //多线程操作
+            List<Future<Map<String, String>>> resultList = new ArrayList<Future<Map<String, String>>>();
+            for (int i=1;i<=1;i++) {
+
+                Future<Map<String, String>> future = executorService.submit(new DealData(i));
+                resultList.add(future);
+            }
+
+            // 遍历任务的结果
+            for (Future<Map<String, String>> fs : resultList) {
+                try {
+                    Map<String, String> map = fs.get();
+                    if (map != null && map.size() > 0) {
+                        System.out.println("线程组执行过程中异常："+ map.toString());
+                    }
+                } catch (Exception e) {
+                    System.out.println("线程组执行过程中异常：{}"+ e);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
+}
